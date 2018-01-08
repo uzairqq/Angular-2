cb6c511aadbcc67c45940e648610fe31a99ce995
uzaiqq <uzair.qq@outlook.com>
2018-01-08 13:02:46 +0500

    created an export class to ensure that other components use this class as a component

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from "@angular/core";
 
-
 @Component({
-  selector:"pm-root",
-  template:`
-  <div><b><h1>{{pageTitle}}</h1></b></div>
-  <div><b>My First Component</b></div>
-  `
+selector:'pm-root',
+template:
+`
+<div><h1>{{pageTitle}}</h1></div>
+<div>My First Component</div>
+`
 })
 
+
 export class AppComponent{
-  pageTitle:string="Acme Product Managment"
+  pageTitle:string="Acme Product Management"
 }
