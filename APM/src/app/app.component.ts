c98c144fb3ae19937ff9550f404f8d7e0bdc640c
uzaiqq <uzair.qq@outlook.com>
2018-01-08 15:00:59 +0500

    Created a product-list folder product list html

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from "@angular/core";
 
 @Component({
-selector:'pm-root',
+selector:'am-root',
 template:
 `
 <div><h1>{{pageTitle}}</h1></div>
 <div>My First Component</div>
 `
 })
-
-
 export class AppComponent{
   pageTitle:string="Acme Product Management"
 }
